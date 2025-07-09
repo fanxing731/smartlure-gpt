948cecd2db483abb2731afed0079a9368653a658
fanxing731 <56124412+fanxing731@users.noreply.github.com>
2025-07-08 19:30:02 -0500

    Add files via upload

--- api/gpt.js
+++ api/gpt.js
@@ -1,3 +1,9 @@
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
 export default async function handler(req, res) {
   const apiKey = process.env.OPENAI_API_KEY;
   const gptRes = await fetch("https://api.openai.com/v1/chat/completions", {
